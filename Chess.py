fc2930d39b9ea6f53160c60bc1c46a4ab379324c
Green <aidan.green2@oregonstate.edu>
2020-06-18 11:14:40 -0700

    Added starting funcs

--- Chess.py
+++ Chess.py
@@ -1,6 +1,7 @@
 import os
 import array
 import numpy
+import random
 
 
 rows = 7
@@ -13,11 +14,11 @@ def initializeboard(rows, columns):
     return board
 
 def renderboard(rows, columns):
+    os.system('cls')
     for i in range(rows):
         for j in range(columns):
                 print(board[i][j], end='   ')
         print("\n")
-
 
 def placepieces(rows, columns):
     for i in range(2):
@@ -28,8 +29,17 @@ def placepieces(rows, columns):
         for j in range(columns):
             f = rows - (i+1)
             board[f][j] = 'X'
-    
-    
+
+def choosestarter():
+    global startingplayer
+    players = ['X','O']
+    startingplayer = random.choice(players)
+    return startingplayer
+
+def startgame(starter):
+    print('Player ' + starter + ', you go first!')
+
 initializeboard(rows, columns)
 placepieces(rows, columns)
 renderboard(rows, columns)
+startgame(choosestarter())
